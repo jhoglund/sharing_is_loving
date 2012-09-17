3cf64eac7192b0d59e7f6a508dd7c60dfe722098
Jonas Höglund <jonas.hoglund@fjordnet.com>
2012-09-17 21:03:11 +0200

    init

---
+++ sharing_is_loving/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SharingIsLoving::Application.config.secret_token = 'e5975b99ddfa698446684777bd09b504457211d380cf0f865ea997e0a24831d2b4cee79b0bc3063fe4dc6bd9b52ba32bc7935916184e02ddce08c346f6f96538'
