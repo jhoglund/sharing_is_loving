a4d5b93f63639e7a6d6f747dbde0431142ffe4b3
Jonas Höglund <jonas.hoglund@fjordnet.com>
2012-09-19 13:43:10 +0200

    Misc

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ if defined?(Bundler)
 end
 
 module SharingIsLoving
-  class Application < Rails::Application
+  class Application < Rails::Application    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
