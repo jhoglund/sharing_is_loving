fb996783169c7689bca50f51a42fb7ef49ca5434
jhoglund <jonas@stixy.com>
2013-11-26 08:28:46 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ SharingIsLoving::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.action_mailer.default_url_options = { :host => 'morning-basin-5184.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'sharing-is-loving.herokuapp.com' }
 end
